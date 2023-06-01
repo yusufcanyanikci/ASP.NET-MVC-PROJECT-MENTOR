6d71f0996a72626740d2400d336ee51f165819c4
Yusufcan Yanıkcı <yupoyanucuk@gmail.com>
2023-06-01 20:21:34 +0300

    ASP.NET-MVC-PROJECT-MENTOR

---
+++ ASP.NET-PROJECT-MENTOR/project/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using project.Models;
+
+namespace project.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Courses()
+    {
+        return View();
+    }
+
+    public IActionResult Traniers()
+    {
+        return View();
+    }
+
+    public IActionResult Events()
+    {
+        return View();
+    }
+
+    public IActionResult Pricing()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+
+    }
+
+    public IActionResult DropDown()
+    {
+        return View();
+
+    }
+
+        public IActionResult cınarınamınaindiriyim()
+    {
+        return View();
+
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
